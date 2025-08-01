379a0707f02acd14f91258a2fe080e7190f84972
ozwol17 <ujjwal1917@gmail.com>
2025-08-01 13:14:42 +0100

    Update app.py

--- crime_dashboard/app.py
+++ crime_dashboard/app.py
@@ -5,25 +5,25 @@ st.title("📊 Warwickshire Crime Dashboard")
 st.markdown("---")
 
 st.header("1. Monthly Trends of Top 5 Crime Types")
-st.image("top 5 crime types.png", use_container_width=True)
+st.image("top_5_crime_types.png", use_container_width=True)
 
 st.header("2. Top 20 Locations with Most Crimes")
-st.image("top 20 locations with most crimes.png", use_container_width=True)
+st.image("top_20_locations_with_most_crimes.png", use_container_width=True)
 
 st.header("3. Confusion Matrix for Model Performance")
-st.image("confusion matrix.png", use_container_width=True)
+st.image("confusion_matrix.png", use_container_width=True)
 
 st.header("4. Total Crime Count by Type")
-st.image("total crime count.png", use_container_width=True)
+st.image("total_crime_count.png", use_container_width=True)
 
 st.header("5. Boxplot of Monthly Crime Counts")
 st.image("boxplot.png", use_container_width=True)
 
 st.header("6. Monthly Crime Counts by Type")
-st.image("monthly crime counts by type.png", use_container_width=True)
+st.image("monthly_crime_counts_by_type.png", use_container_width=True)
 
 st.header("7. Monthly Crime Trends by Crime Type")
-st.image("monthly crime type in.png", use_container_width=True)
+st.image("monthly_crime_trends_by_crime_type.png", use_container_width=True)
 
 st.header("8. Crime Type Distributions")
-st.image("crime type distributions.png", use_container_width=True)
+st.image("crime_type_distributions.png", use_container_width=True)
